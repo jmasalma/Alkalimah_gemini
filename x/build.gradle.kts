bc2b10cbec24bcdb46258218173b4ebfad045226
jmasalma <49733702+jmasalma@users.noreply.github.com>
2025-12-20 06:25:33 -0400

    Delete x directory

--- x/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    // These versions must match your Android Studio / Gradle environment
-    id("com.android.application") version "8.3.2" apply false
-    id("com.android.library") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("com.google.dagger.hilt.android") version "2.57.1" apply false
-}
